16f753496c3dbc72b42a3b292b25cbd372dce3c8
Ruslan <Giovingo92@gmail.com>
2023-05-14 13:32:58 +0300

    add basics styled

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+
+import { Container, Section } from '../index';
+
+export class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <Container>
+        <Section></Section>
+      </Container>
+    );
+  }
+}
